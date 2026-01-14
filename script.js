bf0de7d0eb98e94be8d2d36ba57bd541758aa640
Phan Lê Minh <166001756+LeMinhPhan@users.noreply.github.com>
2026-01-14 20:59:36 +0700

    update cv

--- script.js
+++ script.js
@@ -115,10 +115,14 @@ skillProgressElements.forEach(skillProgress => {
   updateProgressBar();
 });
 
-let banner = document.querySelector('.banner');
 let canvas = document.getElementById('dotsCanvas');
-canvas.width = canvas.offsetWidth;
-canvas.height = canvas.offsetHeight;
+if (!canvas) {
+  canvas = document.createElement('canvas');
+  canvas.id = 'dotsCanvas';
+  document.body.appendChild(canvas);
+}
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 const dots = [];
 const arrayColors = ['#d5c900', '#545454', '#596d91', '#bb5a68', '#696541'];
@@ -139,12 +143,12 @@ const drawDots = () => {
     })
 }
 drawDots();
-banner.addEventListener('mousemove', (event) => {
+document.addEventListener('mousemove', (event) => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawDots();
     let mouse = {
-        x:  event.pageX - banner.getBoundingClientRect().left,
-        y:  event.pageY - banner.getBoundingClientRect().top
+        x:  event.clientX,
+        y:  event.clientY
     }
     dots.forEach(dot => {
         let distance = Math.sqrt((mouse.x - dot.x) ** 2 + (mouse.y - dot.y) ** 2);
@@ -158,15 +162,15 @@ banner.addEventListener('mousemove', (event) => {
         }
     })
 })
-banner.addEventListener('mouseout', () => {
+document.addEventListener('mouseout', () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawDots();
 })
 window.addEventListener('resize', () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    canvas.width = banner.offsetWidth;
-    canvas.height = banner.offsetHeight;
-    dot = [];
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    dots.length = 0;
     for (let index = 0; index < 50; index++) {
         dots.push({
             x:  Math.floor(Math.random() * canvas.width),

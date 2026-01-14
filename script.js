83387d38917636c2f63b8ab3908d28be8d3d30a7
Phan Lê Minh <166001756+LeMinhPhan@users.noreply.github.com>
2026-01-14 21:40:12 +0700

    update background

--- script.js
+++ script.js
@@ -184,14 +184,16 @@ window.addEventListener('resize', () => {
 const $next = document.querySelector('.next');
 const $prev = document.querySelector('.prev');
 
-$next.addEventListener('click', () => {
-    const items = document.querySelectorAll('.item');
-    document.querySelector('.slide'). appendChild(items[0]);
-}, 
-);
-
-$prev.addEventListener('click', () => {
-    const items = document.querySelectorAll('.item');
-    document.querySelector('.slide'). prepend(items[items.length - 1]);
+if ($next && $prev) {
+    $next.addEventListener('click', () => {
+        const items = document.querySelectorAll('.item');
+        document.querySelector('.slide').appendChild(items[0]);
     }, 
-);
+    );
+
+    $prev.addEventListener('click', () => {
+        const items = document.querySelectorAll('.item');
+        document.querySelector('.slide').prepend(items[items.length - 1]);
+    }, 
+    );
+}
